e384b9ef765384a714d4bb113a64d7d19205146c
alifadhul68 <107270551+alifadhul68@users.noreply.github.com>
2023-01-03 22:45:49 +0300

    Delete web directory

--- web/hotel_hub/hotels/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'hotels'
-urlpatterns = [
-    path('hotels/', views.hotel_list, name='hotel_list'),
-    path('add/', views.add, name='add'),
-]
